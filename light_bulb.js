d89d3e0c766b5770900c422c35f9af64584cbb54
Dimpyved <91733438+Dimpyved@users.noreply.github.com>
2023-01-03 20:48:06 +0530

    Add files via upload

---
+++ light_bulb.js
@@ -0,0 +1,46 @@
+Status = "";
+LightBulb_image = "";
+object = [];
+
+function preload(){
+    LightBulb_image = loadImage('Light Bulb.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(LightBulb_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(LightBulb_image,0,0,640,350);
+
+    if(status != "")
+    {
+        for (i= 0; i < object.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects.y, objects[i].width, objects[i].height);
+        }
+    }
+}
